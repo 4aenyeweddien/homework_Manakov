73e8a60b9b52729a10be691217d68c667f4363e4
Виктор Манаков <manackov.vitek@gmail.com>
2024-06-28 18:14:25 +0300

    fix

--- src/widget.py
+++ src/widget.py
@@ -1,15 +1,15 @@
-import masks
+from src import masks
 
 
-def mask_account_card(user_input: str) -> str:
-    """Принимает строку и возвращает ее замаскированной"""
-    if len(user_input.split()[-1]) == 16:
-        new_account = masks.get_mask_card_number(user_input.split()[-1])
-        disguised_account = f"{user_input[:-16]}{new_account}"
+def mask_account_card(number_card_account: str) -> str:
+    """Принимает строку и возвращает замаскированную"""
+    if len(number_card_account.split()[-1]) == 16:
+        new_account = masks.get_mask_card_number(number_card_account.split()[-1])
+        disguised_account = f"{number_card_account[:-16]}{new_account}"
         return disguised_account
-    elif len(user_input.split()[-1]) == 20:
-        new_card = masks.get_mask_account(user_input.split()[-1])
-        disguised_card = f"{user_input[:-20]}{new_card}"
+    elif len(number_card_account.split()[-1]) == 20:
+        new_card = masks.get_mask_account(number_card_account.split()[-1])
+        disguised_card = f"{number_card_account[:-20]}{new_card}"
         return disguised_card
 
 
@@ -26,10 +26,3 @@ result_card = mask_account_card(user_card)
 result_date = get_data(user_date)
 print(result_card)
 print(result_date)
-
-
-
-
-
-
-
