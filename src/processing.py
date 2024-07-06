58f44441d25582595c5ee5efd1c97230c4c0c481
Виктор Манаков <manackov.vitek@gmail.com>
2024-07-06 23:05:39 +0300

    fix test_processing

--- src/processing.py
+++ src/processing.py
@@ -19,12 +19,10 @@ def sort_by_date(list_dict: list[dict[str, Any]], reverse: bool = True) -> list[
 
 
 # if __name__ == '__main__':
-#     list_dict = [
-#         {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-#         {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-#         {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-#         {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
-#                  ]
+#     list_dict = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+#             {'id': 939719570, 'state': 'EXECUTED', 'date': '2019-07-03T02:08:58.425572'},
+#             {'id': 594226727, 'state': 'CANCELED', 'date': '2019-07-03T21:27:25.241689'},
+#             {'id': 615064591, 'state': 'CANCELED', 'date': '2019-07-03T08:21:33.419441'}]
 #
 #     result1 = filter_by_state(list_dict)
 #     print(result1)
